8916cf4606c393bc68669551081f7ce2dbbaa480
AntoineDeleplanque <antoine.deleplanque@epsi.fr>
2014-05-28 02:56:29 +0200

    ProjectBase

---
+++ DigiCode/DigiCode/SalleService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace DigiCode
+{
+    class SalleService
+    {
+        public static IEnumerable<SalleEntity> LoadSalleList()
+        {
+            using (DbModelContainer ctx = new DbModelContainer())
+            {
+                return ctx.SalleEntityJeu.ToList();
+            }
+        }
+    }
+}

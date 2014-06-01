ca2e1165ba78ba24b44163572a1f3a584732dcc9
AntoineDeleplanque <antoine.deleplanque@epsi.fr>
2014-06-01 22:29:34 +0200

    UpdateToPutOnClient

--- DigiCode/DigiCode/MainWindow.xaml.cs
+++ DigiCode/DigiCode/MainWindow.xaml.cs
@@ -1,32 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DigiCode
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
             IEnumerable<SalleEntity> mySalles = SalleService.LoadSalleList();
             SalleListView.ItemsSource = mySalles;
-            //SalleListView.Items.Add(new Salle());
-            //((Salle)SalleListView.Items[0]).SetUnamedSalle("SALLE");
         }
 
         private void ResetCodeButton_Click(object sender, RoutedEventArgs e)
@@ -39,6 +27,26 @@ namespace DigiCode
                     foreach (SalleEntity salle in SalleListView.SelectedItems)
                     {
                         salle.ResetCode();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pas de salle séléctionnée");
+            }
+        }
+
+        private void ShowCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SalleListView.SelectedItems.Count > 0)
+            {
+                SalleEntity firstOrDefaultSalleEntity = (SalleEntity)((IList<object>)SalleListView.SelectedItems).FirstOrDefault();
+                if (firstOrDefaultSalleEntity != null)
+                {
+                    MessageBoxResult result = MessageBox.Show("Voir le code de cette salle : " + firstOrDefaultSalleEntity.Libelle + "?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        MessageBox.Show(firstOrDefaultSalleEntity.Code.ToString(CultureInfo.InvariantCulture));
                     }
                 }
             }
--- DigiCode/DigiCode/Salle.cs
+++ DigiCode/DigiCode/Salle.cs
@@ -1,25 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common.EntitySql;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Win32;
-
-namespace DigiCode
+﻿namespace DigiCode
 {
     partial class SalleEntity
     {
-        public void SetUnamedSalle(string Libelle)
-        {
-            if (this.Libelle == null)
-            {
-                this.Libelle = Libelle;
-            }
-        }
-
         public void ResetCode()
         {
             using (var DbModelContainer = new DbModelContainer())
